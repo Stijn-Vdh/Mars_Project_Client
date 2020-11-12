16556b433f471394362042ba641c406993d2a75c
Liam <contact@liamd.me>
2020-11-12 15:09:54 +0100

    Searchbar dropdown styling

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -2,6 +2,39 @@ function getMessage() {
     return apiCall("message");
 }
 
+function getEndpoints() {
+    return [
+        {
+            "id": "1",
+             "name": "Home",
+            "available": true,
+            "location": "here",
+            "privateEndpoint": true
+        },
+        {
+            "id": "2",
+             "name": "Home",
+            "available": true,
+            "location": "here",
+            "privateEndpoint": true
+        },
+        {
+            "id": "3",
+             "name": "Home",
+            "available": true,
+            "location": "here",
+            "privateEndpoint": true
+        },
+        {
+            "id": "4",
+             "name": "Home",
+            "available": true,
+            "location": "here",
+            "privateEndpoint": true
+        },
+    ]
+}
+
 function apiCall(uri) {
     const request = new Request(api + uri, {
         method: 'GET',
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,6 +11,8 @@ async function init() {
     // api = `${config.host ? config.host + '/': ''}${config.group ? config.group + '/' : ''}api/`;
 
     document.querySelector('#quick-access').addEventListener('click', openQuickAccess);
+    document.querySelectorAll(".searchbar > input").forEach(el => el.addEventListener('focusin', toggleFocus));
+    document.querySelectorAll(".searchbar > input").forEach(el => el.addEventListener('focusout', toggleFocus));
     loadRecentTrips();
 }
 
@@ -48,4 +50,16 @@ function loadRecentTrips() {
     dummyData.forEach(route => {
         tripContainer.innerHTML += recentTrip(route);
     })
+}
+
+function toggleFocus(e) {
+    let wait = 0;
+    if (e.target.parentNode.classList.contains("active")) {
+        e.target.parentNode.querySelector('ul').style.height = '0';
+        wait = 300;
+    }
+    setTimeout(() => {
+        e.target.parentNode.classList.toggle('active');
+        e.target.parentNode.querySelector('ul').style.height = '';
+    }, wait)
 }
