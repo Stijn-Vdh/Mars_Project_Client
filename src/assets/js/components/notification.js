b46a73667937fe5d5c5e75411bce535032e5561c
Liam <contact@liamd.me>
2020-12-10 16:22:14 +0100

    Reworked notifications

--- src/assets/js/components/notification.js
+++ src/assets/js/components/notification.js
@@ -1,42 +1,55 @@
 "use strict";
 
-const notificationQueue = [];
-let notificationShown = false;
+let notificationId = 0;
 
 function notify(message) {
     sendNotification('success', message);
 }
 
 function warn(message) {
-    sendNotification('warning', message);
+    sendNotification('warn', message);
 }
 
 function error(message) {
-    sendNotification('error', message);
+    sendNotification('danger', message);
 }
 
 function sendNotification(type, message) {
-    notificationQueue.push([type, message]);
-    checkQueue();
-}
+    notificationId++;
+    const thisId = notificationId;
+    const icon = type === 'danger' ? 'alert': type === 'warn' ? 'help': 'checkmark';
 
-function checkQueue() {
-    if (notificationQueue.length > 0 && !notificationShown) {
-        const notification = notificationQueue.shift();
-        document.querySelector('#notification p').innerHTML = notification[1];
-        document.querySelector('#notification').classList.add(notification[0]);
-        document.querySelector('#notification').classList.add('open');
+    document.querySelector('#notifications').innerHTML += `
+    <div id="notification-${thisId}" class="notification ${type}">
+        <ion-icon name="${icon}-outline"></ion-icon>
+        <p>${message}</p>
+    </div>`;
 
-        notificationShown = true;
-        setTimeout(hideNotification, 5000);
-    }
-}
-
-function hideNotification() {
-    document.querySelector('#notification').classList.remove('open')
-    notificationShown = false;
     setTimeout(() => {
-        document.querySelector('#notification').className = '';
-        setTimeout(checkQueue, 1000);
+        document.querySelector(`#notification-${thisId}`).style.transform = 'translateX(0vw)';
+    }, 100);
+
+    setTimeout(() => {
+        hideNotification(thisId);
+    }, 5000)
+}
+
+// function checkQueue() {
+
+//     if (notificationQueue.length > 0 && !notificationShown) {
+//         const notification = notificationQueue.shift();
+//         document.querySelector('#main-notification p').innerHTML = notification[1];
+//         document.querySelector('#main-notification').classList.add(notification[0]);
+//         document.querySelector('#main-notification').classList.add('open');
+
+//         notificationShown = true;
+//         setTimeout(hideNotification, 5000);
+//     }
+// }
+
+function hideNotification(id) {
+    document.querySelector(`#notification-${id}`).style.transform = 'translateX(-100vw)';
+    setTimeout(() => {
+        document.querySelector(`#notification-${id}`).remove();
     }, 300);
 }
